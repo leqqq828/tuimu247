14b2f5a569341554d7a76ae8f3e77406dca44064
leqqq828 <lewiskirven@gmail.com>
2026-07-26 14:40:04 +0700

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -32,5 +32,6 @@ app.get("/api/products", (_req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server đang chạy tại port ${PORT});
+  console.log("Server running on port " + PORT);
 });
+
